456b592a950272751b43ccd5c69205f97774e1cd
mebangit123 <mebannongrum333@gmail.com>
2021-04-29 16:09:29 +0530

    Refractor to pass lambda function as argument

--- LambdaMathOperation/src/com/math/lambda/MathOperation.java
+++ LambdaMathOperation/src/com/math/lambda/MathOperation.java
@@ -5,7 +5,7 @@ public class MathOperation {
 		System.out.println("Math Operation Using Lambda Expression");
 		
 		MathFunction add = Integer::sum;
-		MathFunction sub = Integer::sum;
+		MathFunction sub = (x,y) -> x -y;
 		MathFunction mul = (x, y) -> x * y;
 		MathFunction div = (int x, int y) -> x / y;
 		
@@ -13,8 +13,16 @@ public class MathOperation {
 		System.out.println("Substraction is :"+ sub.calculate(6, 2));
 		System.out.println("Multiplication is :"+ mul.calculate(6, 2));
 		System.out.println("Division is :"+ div.calculate(6, 2));
+		System.out.println("--------------------------------------------");
+		MathFunction.printResult(6, 2, "Addition: ", add);
+		MathFunction.printResult(6, 2, "Substraction", sub);
+		MathFunction.printResult(6, 2, "Multiplication", mul);
+		MathFunction.printResult(6, 2, "Division", div);
 	}
 }
 interface MathFunction {
 	int calculate(int a, int b);
+	static void printResult(int a, int b, String function, MathFunction func) {
+		System.out.println("Result of "+function+ "is: " +func.calculate(a, b));
+	}
 }
